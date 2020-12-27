185d0fe44df7a1a106d1301ce9ef912fecfde0c5
AnDCGI <69905741+AnDCGI@users.noreply.github.com>
2020-12-27 22:04:52 +0530

    Update Clean Unknown Plugins.py

--- Clean Unknown Plugins.py
+++ Clean Unknown Plugins.py
@@ -1,5 +1,7 @@
 # Clean Unknown Plugins
 # Copyright ©  2020  AnD CGI
+# This Script Will Delete Any Unknown Plugins From Maya Scene
+# The List of Deleted Plugins Can Be Seen On Maya Script Editor
 import maya.cmds as cmds
 cmds.delete(cmds.ls(type="unknown"))
 plg_ls = cmds.unknownPlugin(q=True, l=True)
