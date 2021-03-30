59b0826ac1237516eda49beb494a4837a7cbca47
AnDCGI <69905741+AnDCGI@users.noreply.github.com>
2021-03-31 01:33:24 +0530

    Update aiMatte.py

--- aiMatte.py
+++ aiMatte.py
@@ -1,3 +1,4 @@
+# © 2020 AnD CGI This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
 import maya.cmds as cmds
 sel = cmds.ls(ni=1, typ=["mesh", "nurbsSurface", "subdiv"])
 num = len(sel)
