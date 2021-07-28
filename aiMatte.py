00555f9b5e457733c59fe6c9d0d28a85d15b6e88
AnDCGI <69905741+AnDCGI@users.noreply.github.com>
2021-07-28 10:14:04 +0530

    Delete aiMatte.py

--- aiMatte.py
+++
@@ -1,7 +0,0 @@
-# © 2020 AnD CGI This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
-import maya.cmds as cmds
-sel = cmds.ls(ni=1, typ=["mesh", "nurbsSurface", "subdiv"])
-num = len(sel)
-for i in range(0,num):
-	selAttr = sel[i] + ".aiMatte"
-	cmds.setAttr(selAttr, 1)
